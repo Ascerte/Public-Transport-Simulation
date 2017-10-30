6b518d3152b7fb1cf2a0b99c8bcf5d0a663cd11d
Ascerte <tanasasilviu@gmail.com>
2017-10-30 21:19:50 +0200

    Test

--- Public Transport Simulation/Serv_network.h
+++ Public Transport Simulation/Serv_network.h
@@ -5,7 +5,7 @@ public:
 	Serv_network(int stops);
 	~Serv_network();
 	int& operator()(const int index1, const int index2);
-	void makeGraph(/*int graph[10][10], int stops*/);
+	void makeGraph();
 	void printGraph();
 
 private:
